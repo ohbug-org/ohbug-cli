0d0013487d06e48c714442cc1d34e3f19c705810
chenyueban <jasonchan0527@gmail.com>
2020-08-29 14:07:01 +0800

    feat: update the default upload url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ export const LOG_PREFIX = `[OhbugCli]`;
 
 export const TIMEOUT = 30000;
 
-export const DEFAULT_URL = `http://api.ohbug.net/sourceMap/upload`;
+export const DEFAULT_URL = `https://api.ohbug.net/v1/sourceMap/upload`;
